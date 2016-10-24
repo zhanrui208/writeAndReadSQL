829987c21de48a986bae02b65b7acef8b9064d0f
zhanrui208 <271912564@qq.com>
2016-10-24 17:30:03 +0800

    1

---
+++ src/main/java/writeAndReadSQL/DynamicDataSource.java
@@ -0,0 +1,14 @@
+package writeAndReadSQL;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+public class DynamicDataSource   extends AbstractRoutingDataSource{
+
+	@Override
+	protected Object determineCurrentLookupKey() {
+
+		 // 使用DynamicDataSourceHolder保证线程安全，并且得到当前线程中的数据源key  
+		  return DynamicDataSourceHolder.getDataSourceKey();  
+	}
+
+}
