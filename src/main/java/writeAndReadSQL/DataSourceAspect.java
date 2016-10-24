3dd2f725244b9439cece63b09587c73234a051e6
zhanrui208 <271912564@qq.com>
2016-10-24 17:53:26 +0800

    3

--- src/main/java/writeAndReadSQL/DataSourceAspect.java
+++ src/main/java/writeAndReadSQL/DataSourceAspect.java
@@ -1,6 +1,6 @@
 package writeAndReadSQL;
  import org.apache.commons.lang3.StringUtils;  
- import org.aspectj.lang.JoinPoint; 
+import org.aspectj.lang.JoinPoint;
 public class DataSourceAspect {
 	/**
 	 * 在进入Service方法之前执行
---
+++ src/main/java/writeAndReadSQL/test.java
@@ -0,0 +1,13 @@
+package writeAndReadSQL;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext xtx = new ClassPathXmlApplicationContext("class:applicationContext.xml");
+	}
+
+}
